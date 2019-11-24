5649eb3397358261bce4c861b41383718308ea4f
Eli <elimaynard923@gmail.com>
2019-11-24 15:02:28 -0800

    Finally refactored linewrap code

--- src/assets/notes/linewrap.js
+++ src/assets/notes/linewrap.js
@@ -1,108 +1,71 @@
 // Modified from https://github.com/xdamman/js-line-wrap-detector
 
 (function() {
+  'use strict';
 
-  function wrapWords(text, before, after, join = ' ') {
-    var words = text.split(' ');
-    for (let i = 0; i < words.length; i++) {
-      words[i] = before + words[i] + after;
-    }
-    return words.join(join);
-  };
+  function ensureDescendantTextNodesWrapped(parentNode) {
+    /* Given an element containing a bunch of text nodes,
+     * wrap the text noted in a <span class="__linewrap">
+     */
 
-   function wrapWordsInChildElement(el) {
-    if (el.nodeName == '#text') {
-      var words = el.textContent.split(' ');
-      for (var i = 0; i < words.length; i++) {
-        if (words[i].length > 0) {
-          var span = document.createElement('span');
-          span.className = "js-detect-wrap";
-          span.innerText = words[i];
-          el.parentNode.insertBefore(span, el);
-        }
-        if (i < words.length - 1) {
-          el.parentNode.insertBefore(document.createTextNode(" "), el);
-        }
+    // Get a clone of the children array which is NOT in sync with .childNodes
+    const children = Array.from(parentNode.childNodes);
+    for (const childNode of children) {
+      // Don't do anything if already wrapped
+      if (childNode.classList && childNode.classList.contains('__linewrap')) {
+        return;
       }
-      el.parentNode.removeChild(el);
-    } else {
-      if (el.innerText) {
-        el.innerHTML = wrapWords(el.innerText, '<span class="js-detect-wrap">','</span>');
-      }
-    }
-  };
 
-  function wrapWordsInElement(el) {
-    if (el.id && el.id.includes("MathJax")) {
       // Treat MathJax atomically
-      el.classList.add('js-detect-wrap');
-    } else if (!el.firstChild) {
-      wrapWordsInChildElement(el);
-    } else {
-      var siblings = [];
-      var s = el.firstChild;
-      do {
-        siblings.push(s);
-      } while (s = s.nextSibling);
-
-      for (var i = 0; i < siblings.length; i++) {
-        wrapWordsInElement(siblings[i]);
+      if (childNode.id && childNode.id.includes("MathJax")) {
+        childNode.classList.add('__linewrap');
+      } else if (childNode.nodeName === "#text") {
+        const words = childNode.textContent.split(' ');
+        words.forEach((word, wordIdx) => {
+          const span = document.createElement('span');
+          span.className = '__linewrap';
+          span.innerText = word;
+          parentNode.insertBefore(span, childNode);
+          if (wordIdx !== words.length - 1) {
+            parentNode.insertBefore(document.createTextNode(' '), childNode);
+          }
+        });
+        parentNode.removeChild(childNode);
+      } else {
+        ensureDescendantTextNodesWrapped(childNode);
       }
     }
-  }
 
-  var getLines = function(el) {
-    wrapWordsInElement(el);
-    var spans = el.getElementsByClassName('js-detect-wrap');
-
-    var lineOffset = null;
-    var lineHeight = null;
-    var line = [];
-    var lines = [];
-    for (let i = 0; i < spans.length; i++) {
-      let spanHeight = spans[i].getBoundingClientRect().height;
-      let offset = spans[i].offsetTop + spanHeight;
-      if (lineOffset === null) {
-        lineOffset = offset;
-        lineHeight = spanHeight / 2;
-      }
-
-      // When testing if we're on a new line, we account for anomolies (e.g. MathJax)
-      // by allowing items to go back up as well as go a little bit down (0.5em)
-      if (offset > lineOffset + lineHeight / 2) {
-        lines.push(line);
-        line = [];
-        lineOffset = offset;
-        lineHeight = spanHeight / 2;
-      }
-      line.push(spans[i]);
-    }
-    lines.push(line);
-
-    return lines;
   }
 
   function getLastWordOnLineWith(el, paragraph) {
-    // Get the last word on the same line as the given element
-    wrapWordsInElement(paragraph);
-    let spans = paragraph.getElementsByClassName('js-detect-wrap');
+    const elBottom = el.getBoundingClientRect().y + el.getBoundingClientRect().height;
 
-    let elBottom = el.offsetTop + el.getBoundingClientRect().height;
     // Where we consider the start of the "next line"
-    let threshold = elBottom + el.getBoundingClientRect().height / 2;
-    var prevWord;
-    for (let i = 0; i < spans.length - 1; i++) {
-      let word = spans[i];
-      if (word.offsetTop + word.getBoundingClientRect().height > threshold) {
+    // (Doing it this way is useful to work properly with things like e.g. MathJax,
+    // where symbols can just out a little bit below the bottom of the line they're in.)
+    const threshold = elBottom + el.getBoundingClientRect().height / 4;
+
+    // Get the last word on the same line as the given element
+    ensureDescendantTextNodesWrapped(paragraph);
+    const spans = paragraph.getElementsByClassName('__linewrap');
+
+    if (spans.length === 1) return spans[0];
+
+    for (let i = 1; i < spans.length; i++) {
+      const word = spans[i];
+      const prevWord = spans[i-1];
+      const wordBottom = word.getBoundingClientRect().y + word.getBoundingClientRect().height;
+      if (wordBottom > threshold) {
         return prevWord;
       }
-      prevWord = word;
     }
 
-    return paragraph.childNodes[paragraph.childNodes.length - 1];
+    return spans[spans.length - 1];
   }
 
-  window.lastWordInLine = getLastWordOnLineWith;
+  window.getLastWordOnLineWith = getLastWordOnLineWith;
 
 })();
+
 
--- src/assets/notes/notes.js
+++ src/assets/notes/notes.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function findPos(obj) { // From https://www.quirksmode.org/js/findpos.html
   var curleft = curtop = 0;
   if (obj.offsetParent) {
@@ -18,16 +20,17 @@ const NoteRef = {
 function refOnClick(host) {
   const note = document.getElementById(host.getAttribute('to'));
 
-  // Display the note:
-  // If it's already showing,
+  // If the note is already showing,
   const existingClone = document.getElementById(note.id + '-clone');
   if (existingClone) {
     // Then close it
     existingClone.remove();
+
   } else { // Otherwise, open it
+
     // First, we need to find the last word before the next linebreak.
     const paragraph = host.parentNode;
-    const lastWord = lastWordInLine(host, paragraph);
+    const lastWord = window.getLastWordOnLineWith(host, paragraph);
 
     // Make a clone
     const noteClone = note.cloneNode(true);
@@ -42,13 +45,13 @@ function refOnClick(host) {
     // Additionally, place the triangle
     const triang = document.createElement('span');
     triang.className = 'triangle';
-    // Place within note
-    noteClone.appendChild(triang);
-    const xCoordinate = Math.floor(
-      host.offsetLeft - noteClone.offsetLeft +
-        (host.getBoundingClientRect().width - triang.getBoundingClientRect().width) / 2
-    );
-    triang.style.left = xCoordinate;
+    noteClone.appendChild(triang);  // We have to attach it to the DOM pretty early so that .getBoundingClientRect() works
+    const xCoordinate = ( host.getBoundingClientRect().x
+                        - noteClone.getBoundingClientRect().x
+                        + host.getBoundingClientRect().width / 2
+                        - triang.getBoundingClientRect().width / 2);
+    triang.style.left = xCoordinate + 'px';
+
   }
 }
 hybrids.define('note-def', Note);
